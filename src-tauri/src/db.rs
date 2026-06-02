ef6efd217bf1c2d27b28814013560f43c360e81c
Koei32 <205915319+Koei32@users.noreply.github.com>
2026-06-02 17:41:48 +0530

    feat: basic database interactions

---
+++ src-tauri/src/db.rs
@@ -0,0 +1,48 @@
+//! database and things like that
+
+use anyhow::{Ok, Result};
+use sqlx::SqlitePool;
+const DATABASE_URL: &str = "sqlite://userdata/data.db?mode=rwc";
+
+use crate::clipboard::ClipboardEvent;
+
+#[derive(Debug, Clone)]
+pub struct Database {
+    pool: SqlitePool,
+}
+
+impl Database {
+    /// Returns a new database instance connected to `./userdata/data.db`
+    pub async fn new() -> Result<Self> {
+        let pool = SqlitePool::connect(DATABASE_URL).await?;
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        Ok(Self { pool })
+    }
+
+    /// Inserts clipboard event content into the database, returning the event
+    /// on success or an error on failure to insert
+    pub async fn create_entry(&self, event: ClipboardEvent) -> Result<()> {
+        sqlx::query(
+            "
+            INSERT INTO clipboard (type, content) 
+            VALUES (?, ?);
+            ",
+        )
+        .bind(String::from(event.event_type))
+        .bind(event.content)
+        .execute(&self.pool)
+        .await
+        .expect("failed insertion");
+
+        Ok(())
+    }
+
+    /// Gets all clipboard entries
+    pub async fn get_entries(&self) -> Result<Vec<ClipboardEvent>> {
+        let results: Vec<ClipboardEvent> = sqlx::query_as("SELECT * FROM clipboard;")
+            .fetch_all(&self.pool)
+            .await?;
+        Ok(results)
+    }
+}
