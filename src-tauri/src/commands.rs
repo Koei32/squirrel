add76fdba9037e982cb250c786234260e27e953e
Koei32 <205915319+Koei32@users.noreply.github.com>
2026-08-09 21:09:23 +0530

    feat: files copy and paste

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -35,7 +35,7 @@ pub async fn copy_item(
                 .unwrap(),
             )
             .map_err(|e| e.to_string())?,
-        CbEventContent::File(_) => (), // todo
+        CbEventContent::File(files) => cb.set_files(files).map_err(|e| e.to_string())?,
     };
     Ok(())
 }
